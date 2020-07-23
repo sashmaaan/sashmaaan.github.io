d52062dc830726713defc464c80e94be5abce243
Sascha Schmolz <sascha.schmolz@googlemail.com>
2020-07-23 16:57:40 +0700

    'changed'

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -42,13 +42,13 @@ $(document).ready(function() {
 
   materialKit.checkScrollForTransparentNavbar();
 
-  if (window_width >= 768) {
+  // if (window_width >= 768) {
     big_image = $('.page-header[data-parallax="true"]');
     if (big_image.length != 0) {
       $(window).on('scroll', materialKit.checkScrollForParallax);
     }
 
-  }
+  // }
 
 
 });
